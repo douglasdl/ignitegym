43d9758c3002b99ef345a10c5bf3c9412ac35da4
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-07 14:19:05 +0900

    feat: add Input component

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,6 +1,7 @@
-import { Center, Image, Text, VStack } from "@gluestack-ui/themed"
+import { Center, Heading, Image, Text, VStack } from "@gluestack-ui/themed"
 import BackgroundImg from "@assets/background.png"
 import Logo from "@assets/logo.svg"
+import { Input } from "@components/Input"
 
 export function SignIn() {
   return (
@@ -14,12 +15,21 @@ export function SignIn() {
         position="absolute"
       />
 
-      <Center my="$24">
-        <Logo />
-        <Text color="$gray100" fontSize="$sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
+          <Text color="$gray100" fontSize="$sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center gap="$2">
+          <Heading color="$gray100">Acesse a conta</Heading>
+
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" />
+        </Center>  
+      </VStack>
     </VStack>
   )
 }

9ea5a8fd6e025b304f2117997da89705245fd4a7
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-07 15:20:49 +0900

    feat: add the public routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { SignIn } from "@screens/Signin";
+import { SignUp } from "@screens/Signup";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Navigator>
+      <Screen 
+        name="signIn"
+        component={SignIn}
+      />
+      <Screen 
+        name="signUp"
+        component={SignUp}
+      />
+    </Navigator>
+  )
+}

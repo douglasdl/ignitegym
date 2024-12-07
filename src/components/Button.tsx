948a834bfbd8b00f5c0885da3a787ec791600053
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-07 14:43:23 +0900

    feat: add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import  { Button as GluestackButton, Text, ButtonSpinner } from "@gluestack-ui/themed"
+import { ComponentProps } from "react"
+
+type Props = ComponentProps<typeof GluestackButton> & {
+  title: string
+  isLoading?: boolean
+}
+
+export function Button({ title, isLoading = false, ...rest } :Props) {
+  return (
+    <GluestackButton 
+      w="$full" 
+      h="$14"
+      bg="$green700"
+      borderWidth="$0"
+      borderColor="$green500d"
+      rounded="$sm"
+      $active-bg="$green500"
+      disabled={isLoading}
+      {...rest}
+    >
+      {
+        isLoading ? <ButtonSpinner color="$white" /> :
+        <Text color="$white" fontFamily="$heading" fontSize="$sm">
+          {title}
+        </Text>
+      }
+    </GluestackButton>
+  )
+}

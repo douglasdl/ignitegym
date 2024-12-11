adfc2875edb38cb66b3dd240468a7b656b5208af
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-12 07:09:53 +0900

    fix: Home finishings

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -20,7 +20,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group 
             name={item}
-            isActive={groupSelected === item}
+            isActive={groupSelected.toLowerCase() === item.toLowerCase()}
             onPress={() => setGroupSelected(item)} 
           />
         )}

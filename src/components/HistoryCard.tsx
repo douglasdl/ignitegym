79c427e7eec8dbc8cee342c65a18fd746f6e7f35
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-14 20:03:38 +0900

    fix: truncate layout

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -4,8 +4,10 @@ export function HistoryCard() {
 
   return (
     <HStack w="$full" px="$5" py="$4" bg='$gray600' alignItems='center' justifyContent="space-between" rounded='$md' mb="$3">
-      <VStack mr="$5">
-        <Heading color="$white" fontSize="$md" textTransform="capitalize" fontFamily="$heading">Costas</Heading>
+      <VStack flex={1} mr="$5">
+        <Heading color="$white" fontSize="$md" textTransform="capitalize" fontFamily="$heading" numberOfLines={1}>
+          Costas
+        </Heading>
         <Text fontSize='$lg' color='$gray100' numberOfLines={1}>
           Puxada Frontal
         </Text>

0159aa60199b756d30e8c856d191129f345556ad
Douglas Dias Leal <douglas_san@hotmail.com>
2025-02-17 13:25:00 +0900

    feat: add interceptors

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,22 @@
 import axios from 'axios'
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://192.168.3.94:3333'
+});
+
+api.interceptors.request.use((config) => {
+  //console.log("INTERCEPTOR => ", config.data)
+  return config
+}, (error) => {
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use((response) => {
+  console.log("INTERCEPTOR RESPONSE => ", response)
+  return response;
+}, (error) => {
+  console.log("INTERCEPTOR RESPONSE ERROR => ", error)
+  return Promise.reject(error);
 })
+
+export { api };

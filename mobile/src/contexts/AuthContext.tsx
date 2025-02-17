fc336c2a169ecd9e4ee4865e12b307600b9561dc
Douglas Dias Leal <douglas_san@hotmail.com>
2025-02-17 19:10:38 +0900

    feat: add function to get the user data from backend

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
 import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
 import { createContext, ReactNode, useState } from "react"
 
 export type AuthContextDataProps = {
   user: UserDTO
-  signIn: (email: string, password: string) => void
+  signIn: (email: string, password: string) => Promise<void>
 }
 
 type AuthContextProviderProps = {
@@ -13,20 +14,21 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState({
-    id: '1',
-    name: 'Douglas Dias Leal',
-    email: 'douglas_san@hotmail.com',
-    avatar: 'douglas.png'
-  })
+  const [user, setUser] = useState<UserDTO>({} as UserDTO)
 
-  function signIn(email: string, password: string) {
-    setUser({
-      id: '',
-      name: '',
-      email,
-      avatar: ''
-    })
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password });
+      
+      if(data.user) {
+        setUser(data.user);
+        console.log(user);
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+    
   }
 
   return (

bc7372ef50c9fd48795dcedd2e8e81017ea01aaa
Douglas Dias Leal <douglas_san@hotmail.com>
2025-02-18 18:47:27 +0900

    feat: show the default avatar image

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -2,12 +2,13 @@ import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
 import { UserPhoto } from './UserPhoto'
 import { LogOut } from 'lucide-react-native'
 import { useAuth } from '@hooks/useAuth'
+import defaultUserPhotoImg from '@assets/userPhotoDefault.png'
 
 export function HomeHeader() {
   const { user } = useAuth();
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
-      <UserPhoto source={{ uri: 'https://github.com/douglasdl.png'}} w='$16' h='$16' alt='Imagem do usuário' />
+      <UserPhoto source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg } w='$16' h='$16' alt='Imagem do usuário' />
 
       <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">

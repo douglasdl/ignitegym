2539943a3df3aa2e189bf44a85ce16e6eaa280fe
Douglas Dias Leal <douglas_san@hotmail.com>
2025-02-18 18:41:15 +0900

    feat: show the user name

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -1,8 +1,10 @@
 import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
 import { UserPhoto } from './UserPhoto'
 import { LogOut } from 'lucide-react-native'
+import { useAuth } from '@hooks/useAuth'
 
 export function HomeHeader() {
+  const { user } = useAuth();
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
       <UserPhoto source={{ uri: 'https://github.com/douglasdl.png'}} w='$16' h='$16' alt='Imagem do usuário' />
@@ -12,7 +14,7 @@ export function HomeHeader() {
           Olá
         </Text>
         <Heading color="$gray100" fontSize="$md">
-          Douglas Dias Leal
+          {user.name}
         </Heading>
       </VStack>
 
